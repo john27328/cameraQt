6eaff041de351bbff0f6121c9cfb9c18a0e31a5c
Raptev Ivan <raptev@melsytech.com>
2019-09-02 08:51:57 +0300

    реализовал метод моментов

--- ids/diameter.cpp
+++ ids/diameter.cpp
@@ -71,7 +71,32 @@ void Life::diameterSecondMoments()
     //sigma^2_x = int((x - x0)^2 * w(x,y), x, y) / int(w(x,y), x, y)
     //sigma^2_y = int((y - y0)^2 * w(x,y), x, y) / int(w(x,y), x, y)
     //sigma^2_x_y = int((x - x0) * (y - y0) * w(x,y), x, y) / int(w(x,y), x, y)
+
+    //a0 = int(rho^2 * w(rho, phi), rho, phi)
+    //b0 = int(w(rho, phi), rho, phi)
+    //ax =  int((x - x0)^2 * w(x,y), x, y)
+    //ay = int((y - y0)^2 * w(x,y), x, y)
+    //axy =  int((x - x0) * (y - y0) * w(x,y), x, y)
+    //b = int(w(x,y), x, y)
+
+    int ax = 0;
+    int ay = 0;
+    int axy = 0;
+    int b = 0;
+    for (int x = 0; x < range.width; x++) {
+        for (int y = 0; y < range.height; y++) {
+            ax += pow(x-centre.x,2) * (double)frame[x][y];
+            ay += pow(y-centre.y,2) * (double)frame[x][y];
+            axy += (x - centre.x) * (y - centre.y) * (double)frame[x][y];
+            b += (double)frame[x][y];
+        }
+    }
+
     //d = 2 * sqrt(2) * sigma
     //dx = 4 * sigma_x
     //dy = 4 * sigma_y
+
+    int dx = 4 * ax / b;
+    int dy = 4 * ay / b;
+
 }
